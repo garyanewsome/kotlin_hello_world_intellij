8d673e6a1773e3ca0306ebff3b4bc5910b550715
Gary A. Newsome <garyanewsome@gmail.com>
2018-01-26 16:58:41 -0500

    just the basics

---
+++ src/main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>){
+    println("BASIC KOTLIN LEARNING")
+    println("Done with IntelliJ IDEA CE\n")
+    val a: Int = 1
+    val b: Int = 2
+    var c = a + b
+    println("a + b = ${c}\n")
+
+    println("Enter a name:")
+    var name: String?= readLine()
+
+    println("Hello, ${name}, and Welcome.")
+}
